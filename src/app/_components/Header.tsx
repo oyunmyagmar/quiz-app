e63c91ea627771aa99531fe9502f22744e09088b
oyunmyagmar <25LP4018@nest.edu.mn>
2025-11-11 17:46:26 +0800

    aded:

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export const Header = () => {
+  const router = useRouter();
   return (
-    <header className="w-screen fixed z-50 px-6 py-2.5 bg-background text-2xl leading-9 font-semibold border border-input">
-      Quiz app
+    <header className="w-screen fixed z-50 px-6 py-2.5 bg-background border border-input">
+      <div
+        onClick={() => router.push("/")}
+        className="text-2xl leading-9 font-semibold cursor-pointer"
+      >
+        Quiz app
+      </div>
     </header>
   );
 };
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="w-full h-full flex justify-center">
+      <div>LoginPage</div>
+    </div>
+  );
+};
+export default LoginPage;
